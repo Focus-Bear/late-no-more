d1dc23d40b25e6c1d79c788b9e76a2caa1d18d72
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-01-18 10:03:43 -0500

    more tidying

--- src/applescript/dialog.js
+++ src/applescript/dialog.js
@@ -4,38 +4,39 @@ function stringify(buttons) {
     return buttons.map((b) => `"${b}"`).join(', ')
 }
 async function showDialog(title, text, buttons, givingUpAfter = 30) {
-    const defaultButton = buttons.slice(-1)[0];
+    const [defaultButton] = buttons.reverse()  // grab the right most button
 
     const SCRIPT = ` 
---    try
-        set result to ¬ 
-            (display dialog "${text}" ¬ 
+    try
+        set result to ( ¬ 
+            display dialog "${text}" ¬ 
             with title "${title}" ¬
             buttons { ${stringify(buttons)} } ¬
             default button "${defaultButton}" ¬ 
             giving up after ${givingUpAfter} ¬
-            with icon alias (¬
-                (path to application support from user domain as text) & ¬
-                "com.focusbear.latenomore:icon.png") ¬
-                )
+            with icon alias ( ¬
+                ( path to application support from user domain as text ) & ¬
+                "com.focusbear.latenomore:icon.png" ¬
+            ) ¬
+        )
         set buttonReturned to result's button returned
         return buttonReturned
- --    end try`
+    end try`
     return await exec(SCRIPT)
 }
 
 async function askQuestion(question, title, buttons, defaultButton) {
-    if (!defaultButton) [defaultButton] = buttons.reverse() // grab the right most button
+    if (!defaultButton) [defaultButton] = buttons.reverse() 
 
     const SCRIPT = `
     try    
-        set result to (¬
+        set result to ( ¬
             display dialog "${question}" ¬
             default answer "\n\n\n\n" ¬
             with title "${title}" ¬
             buttons { ${stringify(buttons)} } ¬
             default button "${defaultButton}" ¬
-            )
+        )
         set hold to result
         set buttonReturned to button returned of result
         set userInput to text returned of hold

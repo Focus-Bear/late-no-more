39d9482f4671a7a909d3afaaf0d570c73750dac7
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-03-08 10:01:19 -0500

    barking now respects bark window

--- src/bark.js
+++ src/bark.js
@@ -1,4 +1,5 @@
 const say = require('./applescript/say.js')
+const { readSettings } = require('./applescript/fs.js')
 
 const {
     DIALOG_STAGES,
@@ -9,7 +10,33 @@ const {
     LOOK_AHEAD_MINUTES,
 } = require('../config.js')
 
-let barking = false
+let barking = false,
+    barkTime = null
+
+async function setBarkTime() {
+    const { barkStartTime, barkEndTime } = await readSettings()
+    barkTime = { startTime: barkStartTime, endTime: barkEndTime }
+
+    console.log({ barkTime })
+}
+setBarkTime()
+
+function checkIfBarkTime() {
+    const [startHr, startMin] = barkTime.startTime.split(':')
+    const [endHr, endMin] = barkTime.endTime.split(':')
+
+    const startAt = new Date()
+    startAt.setHours(startHr)
+    startAt.setMinutes(startMin)
+
+    const endAt = new Date()
+    endAt.setHours(endHr)
+    endAt.setMinutes(endMin)
+
+    const rn = new Date(Date.now())
+
+    return rn >= startAt && rn <= endAt
+}
 
 function startBarking(evt) {
     console.log('Starting barks...')
@@ -17,11 +44,16 @@ function startBarking(evt) {
 
     if (barking) {
         // avoid multiple barks
-        clearInterval(barking);
+        clearInterval(barking)
     }
 
     barking = setInterval(async () => {
         console.log('In barking interval...')
+        const shouldBark = checkIfBarkTime()
+        if (!shouldBark) {
+            stopBarking()
+            return
+        }
         const randomIndex = Math.floor(Math.random() * VERBAL_ALERTS.length),
             dialog = VERBAL_ALERTS[randomIndex],
             preamble = "Meeting, '" + evt.summary + "'.",
@@ -37,13 +69,12 @@ function stopBarking() {
     barking = false
 }
 
-function getState(){
+function getState() {
     return barking
 }
 
 module.exports = {
     start: startBarking,
     stop: stopBarking,
-    getState
-
+    getState,
 }

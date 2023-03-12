1c425e3cbfcf69ad386a9fda8753a2061a08fda2
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-03-12 09:21:11 -0400

    moved trigger handling to its own file

---
+++ src/scripts/defaultEvent/fbTrigger.js
@@ -0,0 +1,44 @@
+const open = require('../../applescript/open.js')
+
+function extractLine(str) {
+    const lines = str.split('\n')
+    if (!lines?.length) return str
+
+    const [hit] = lines.filter((x) => x.includes('fbtrigger:'))
+
+    const [final] = hit.trim().split(':').reverse()
+    return final
+}
+
+function getEventDuration(event) {
+    const startDate = new Date(event.startDate)
+    const endDate = new Date(event.endDate)
+    const duration = (endDate.getTime() - startDate.getTime()) / 1000 / 60
+    return duration
+}
+
+function checkForTrigger(evt) {
+    const { summary, description } = evt
+
+    const toLookAt = [summary, description]
+
+    for (const str of toLookAt) {
+        if (!str.includes('fbtrigger:')) {
+            continue
+        }
+        const trigger = extractLine(str)
+        return trigger
+    }
+    return false
+}
+
+export default function handleTrigger(evt) {
+    const trigger = checkForTrigger(evt)
+    if (!trigger) return
+
+    console.log(`📍 Found trigger "${trigger}", opening...`)
+    const duration = getEventDuration(evt)
+    const url = `focusbear://start-activity?activity_id=${trigger}&duration_seconds=${duration}`
+
+    open(url)
+}

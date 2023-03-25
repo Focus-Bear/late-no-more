18819a6ddb6dba2fd5d4261e0e255c8cffbee682
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-03-25 11:21:42 -0400

    re-implemented event handling logic

--- src/scripts/defaultEvent/index.js
+++ src/scripts/defaultEvent/index.js
@@ -6,15 +6,28 @@ module.exports = function (evt, now) {
     const events = require('../../events'),
         { delta, imminent, soon } = calculateProximity(evt, now)
 
+    if (events.has('active', evt)) {
+        // if the even has already been imminent,
+        // we won't handle it again
+        return
+    }
+
     if (delta && imminent) {
-        events.remove('looming', evt)
+        // if we're within the 2 minute window,
+        // move the event to active and let the user know
         events.add('active', evt)
         notifyUser(evt)
         return
     }
 
-    if (!soon && events.has('looming', evt)) return
+    if (!soon || events.has('looming', evt)) {
+        // we've already notified the user
+        // so we're going to do nothing
+        return
+    }
 
+    // if this is our first time seeing the event
+    // add it to looming so it can be ignored
     events.add('looming', evt)
     warnUser(evt)
 }

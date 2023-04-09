e20a0178e11b6d43866dc399e976360333ad5bfd
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-04-09 16:33:40 -0400

    parameterized say

--- src/applescript/say.js
+++ src/applescript/say.js
@@ -1,5 +1,40 @@
-const exec = require("./exec.js")
-module.exports = async function say(dialog) {
-  const SCRIPT= `say \"${dialog}\"`
-  await exec(SCRIPT)
-};
+const exec = require('./exec.js')
+
+module.exports = async function say(dialog, options = {}) {
+    const defaultOptions = {
+        using: 'Karen',
+        speakingRate: 150,
+        pitch: 50,
+        volume: 0.5,
+        modulation: 2,
+        waitForCompletion: true,
+    }
+
+    const optionStrings = {
+        using: (value) => ` using "${value}"`,
+        speakingRate: (value) => ` speaking rate ${value}`,
+        pitch: (value) => ` pitch ${value}`,
+        modulation: (value) => ` modulation ${value}`,
+        volume: (value) => ` volume ${value}`,
+        stopCurrentSpeech: (value) => (value ? ` stopping current speech` : ''),
+        waitForCompletion: (value) =>
+            value ? ` waiting until completion true` : '',
+        saveTo: (value) => ` saving to "${value}"`,
+    }
+
+    const combinedOptions = { ...defaultOptions, ...options }
+
+    let SCRIPT = `say "${dialog}"`
+
+    for (const key in combinedOptions) {
+        const option = combinedOptions?.[key]
+        if (option === undefined) continue
+
+        if (optionStrings.hasOwnProperty(key)) {
+            const optionValue = combinedOptions[key]
+            SCRIPT += optionStrings[key](optionValue)
+        }
+    }
+
+    await exec(SCRIPT)
+}

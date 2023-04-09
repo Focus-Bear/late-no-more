237ae64cf0ff76fda2995351d19627fb433b3b45
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-04-09 19:53:21 -0400

    fixed dialog timeout

--- src/scripts/defaultEvent/notify.js
+++ src/scripts/defaultEvent/notify.js
@@ -6,7 +6,7 @@ const {
     ALERT_WINDOW_GIVEUP_TIMEOUT_MINUTES,
 } = require('../../../config.js')
 
-const giveUpAfter = ALERT_WINDOW_GIVEUP_TIMEOUT_MINUTES
+const giveUpAfter = ALERT_WINDOW_GIVEUP_TIMEOUT_MINUTES * 60
 
 async function notifyUser(evt) {
     console.log(`🚨 Notifying user about '${evt.summary}' @ ${evt.startDate}`)
